3f94e565c5298e9d959c620915132b2635e90d76
Melony Franchini <meltravelz@gmail.com>
2023-06-19 21:29:07 -0600

    set up early morning tech challenge

---
+++ 7_flattener.rb
@@ -0,0 +1,28 @@
+################ Instructions ################ 
+# In Ruby, there is a built in method/function to flatten arrays, 
+# meaning it makes them one-dimensional: 
+
+# nums = [1, 2, 3, [[4], 5], [[[6]]]]
+# nums.flatten
+#=> [1, 2, 3, 4, 5, 6]
+
+# Your goal is to recreate this functionality without using the built in method/function. 
+# You will be given a deeply nested array, or multi-dimensional array, 
+# that will look similar to the examples below:
+
+# nums = [1, 2, 3, [[4], 5], [[[6]]]]
+# words = ["hi", "this is", [[["string"], "that is very"], [[[["nested"]]]]]]
+
+# The contents of the array is not significant. Your method/function should take an input 
+# of a multi-dimensional array and output a one-dimensional array. 
+# More simply put, remove the deeply nested brackets to return a single array.
+
+################ Clarification Questions ################ 
+
+################ Match ################ 
+
+################ Pseudocode ################ 
+
+################ First Attempt ################ 
+
+################ Alternative Solutions ################ 
