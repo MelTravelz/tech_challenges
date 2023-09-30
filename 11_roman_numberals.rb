6f60686e00af2cda96d9725485f3c02cb5f497bf
Melony Franchini <meltravelz@gmail.com>
2023-09-30 15:41:37 -0600

    add: first attempt almost finished

--- 11_roman_numberals.rb
+++ 11_roman_numberals.rb
@@ -20,6 +20,10 @@
 # IV=4
 # I=1
 
+# Example solution: 
+# to_roman(128)
+# => "CXXVIII"
+
 ################ Clarification Questions ################ 
 # 1. confirm: the input is an integer or might it occationally be a float, in which case round up or down?
 # 2. confirm: the return is a single string with Roman Numberal Characters
@@ -35,13 +39,47 @@
 # then, we'll iterate through the dictionary hash & pry in
 # i'm thinking we'll need to do some type of substraction to whittle away at the original integer...but we'll see.
 
+# Ok, no that I'm in the method, it looks like we'll need a loop so the number can be subtracted each time until it's zero
+
 ################ First Attempt ################ 
 
+ROMANS = {
+  "M" => 1000,
+  "CM" => 900,
+  "D" => 500,
+  "CD" => 400,
+  "C" => 100,
+  "XC" => 90,
+  "L" => 50,
+  "XL" => 40,
+  "X" => 10,
+  "IX" => 9,
+  "V" => 5,
+  "IV" => 4,
+  "I" => 1
+}
+
+def to_roman(num)
+  roman_numeral = ""
+
+  ROMANS.each do |roman, value|
+    while num >= value
+      roman_numeral += roman
+      num -= value
+    end
+  end
+
+  p roman_numeral
+end
 
 
 
 
+to_roman(55)
+#=> expecting "LV"
 
+to_roman(54)
+#=> expecting "LIV"
 
 ################ Alternative Solutions ################ 
 
